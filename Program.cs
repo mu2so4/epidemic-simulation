3d7df56a62b27ca5334a0868702af72ffd07627b
mu2so4 <b4.orangutang@gmail.com>
2024-06-26 13:21:40 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace monte_carlo_simulation {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
